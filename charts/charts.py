2208fccffd9ec98278d1e047c5c55de4b692765b
Michael Bautista <msbautistaroa@gmail.com>
2023-03-13 23:19:25 -0600

    grafico pie

---
+++ charts/charts.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt 
+
+def generate_pie_chart():
+    labels = [ 'A','B','C']
+    values = [200,34,120]
+
+    fig, ax = plt.subplots()
+    ax.pie(labels=labels, x=values)
+    plt.savefig ('pie2.png')
+    plt.close()
+
