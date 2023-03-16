b6f62c112f53d76932dc40c3971a8b9768959f50
Michael Bautista <msbautistaroa@gmail.com>
2023-03-16 16:04:14 -0600

    uvicorn

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,16 @@
 import store 
+from fastapi import FastApi
+
+app = FastApi()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact')
+def get_list():
+    return {'name': 'Platzi'}
+
 
 def run():
     store.get_categories()
